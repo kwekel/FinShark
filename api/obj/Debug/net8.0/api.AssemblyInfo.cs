502dd58fd2730e27e772410481b2442b6a8860fe
unknown <stefanhabets92@gmail.com>
2024-07-01 18:02:32 +0200

    Added async

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e8369f867637d49589adac4717b31cce42b70d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7cee3c6122bb25f452e3b2858caf1c85634da7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
