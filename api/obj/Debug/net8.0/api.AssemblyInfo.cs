26c287881338ee25f6570c7801bdbbf57a65f44f
unknown <stefanhabets92@gmail.com>
2024-07-06 18:19:39 +0200

    Added jwt

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11c28bace6861cb849cb0a022bbcc264d664af87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa80d6151e14b8ddb99e8842654b49e7278237d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
